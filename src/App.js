645f9912ec15d63b8296b79730668660a760e67d
Anna Podsiadlo <annamagdalena.podsiadlo@gmail.com>
2021-09-10 23:00:14 +0200

    Add progress from day 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import {Switch, Route} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+import MainLayout from './components/layout/MainLayout/MainLayout';
+import Home from './components/views/Homepage/Homepage';
+import About from './components/features/About/About';
+import Projects from './components/features/Projects/Projects';
+import Contact from './components/features/Contact/Contact';
+
+class App extends React.Component {
+  render() {
+    return(
+      <MainLayout>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about-me' exact component={About} />
+          <Route path='/projects' exact component={Projects} />
+          <Route path='/contact' exact component={Contact} />
+        </Switch>
+      </MainLayout>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router} from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/global.scss';
+
+const Root = () => (
+  <Router>
+    <App />
+  </Router>
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Root />,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
